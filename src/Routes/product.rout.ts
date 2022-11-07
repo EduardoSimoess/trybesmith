fb89914e468b580ff1ede0325f97634b63452c15
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-11-07 11:11:39 -0300

    req06

--- src/Routes/product.rout.ts
+++ src/Routes/product.rout.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import midlleware from '../midllewares/product.validation';
 
 const router = Router();
 
 const productController = new ProductController();
 
-router.post('/', productController.create.bind(productController));
+router.post(
+  '/', 
+  midlleware.nameValidation, 
+  midlleware.amountValidation,
+  productController.create.bind(productController),
+);
 router.get('/', productController.getAll.bind(productController));
 
 export default router;
