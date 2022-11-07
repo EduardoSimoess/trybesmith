be7c97d0c3e7a3a3203a760e3bbb25007decae77
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-11-07 11:31:50 -0300

    req 07

--- src/Routes/user.rout.ts
+++ src/Routes/user.rout.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import userMidllewares from '../midllewares/user.validation';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/', userController.login.bind(userController));
+router.post(
+  '/', 
+  userMidllewares.usernameValidation,
+  userMidllewares.passwordValidation,
+  userMidllewares.levelValidation,
+  userMidllewares.classeValidation,
+  userController.login.bind(userController),
+);
 
 export default router;
